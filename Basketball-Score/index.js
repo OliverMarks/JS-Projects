6e2426fc7e40f4ed9c7fa1fe5c915f5965489c6e
oilythighs <oly.marks@gmail.com>
2022-12-09 17:49:01 +1000

    winning team border added

--- Basketball-Score/index.js
+++ Basketball-Score/index.js
@@ -1,12 +1,12 @@
 let homeScore = document.getElementById("homePoints")
 let awayScore = document.getElementById("awayPoints")
-let homeBox = document.getElementsByClassName("homeBox");
-let awayBox = document.getElementsByClassName("awayBox");
+let homeBox = document.querySelector(".homeBox");
+let awayBox = document.querySelector(".awayBox");
 
+console.log(homeBox)
 
 homeScore = 0 
 awayScore = 0
-
 
 
 // functions to increase scores for home and away 
@@ -15,59 +15,65 @@ function hPlusOne() {
     
      homeScore += 1 
      homePoints.textContent = homeScore
+     highlight()
 }
 
 function hPlusTwo() {
     
     homeScore += 2 
     homePoints.textContent = homeScore
+    highlight()
 }
 
 function hPlusThree() {
     
     homeScore += 3 
     homePoints.textContent = homeScore
+    highlight()
 }
 
 function aPlusOne() {
     
     awayScore += 1 
     awayPoints.textContent = awayScore
+    highlight()
 }
 
 function aPlusTwo() {
    
    awayScore += 2 
    awayPoints.textContent = awayScore
+   highlight()
 }
 
 function aPlusThree() {
    
    awayScore += 3 
    awayPoints.textContent = awayScore
+   highlight()
 }
 
 
 // add yellow highlight to team in the lead 
 
-function winning() {
+function highlight() {
     if (homeScore > awayScore) { 
-    homeBox.setAttribute("id","winning");
-    awayBox.removeAttribute("winning"); 
+    homeBox.classList.add("winning");
+    awayBox.classList.remove("winning"); 
+    
 }
 
 else if (homeScore < awayScore) {
-    homeBox.removeAttribute("winning");
-    awayBox.setAttribute("id","winning"); 
+    homeBox.classList.remove("winning");
+    awayBox.classList.add("winning"); 
  }
 
- else (homeScore == awayScore) 
-    homeBox.removeAttribute("winning");
-    awayBox.removeAttribute("winning");
-    }
+ else  {
+    homeBox.classList.remove("winning");
+    awayBox.classList.remove("winning");
+ }
+}
 
-
-winning(); 
 
 
 // function to reset scores on 
