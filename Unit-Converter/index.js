3279c353055ce8997d4cdf72706ce655ab6f6e15
oilythighs <oly.marks@gmail.com>
2022-12-19 17:40:46 +1000

    unit converter

--- Unit-Converter/index.js
+++ Unit-Converter/index.js
@@ -1,5 +1,5 @@
 
-//variables 
+
 
 
 
@@ -12,27 +12,25 @@ let massConversionResult = document.getElementById("massConversion")
 let input = document.getElementById("input")
 
 
-// input.value as parameter for all functions?
 
-
-
-
+// On click of convert button calls all conversion functions
 convertBtn.addEventListener("click", function(){
+    // conversion(input.value)
     lengthConversion(input.value)
     volumeConversion(input.value)
     massConversion(input.value)
 })
 
-
+// on use of up and down arrows in the input field also calls conversion functions w/o need to refresh
+// Would like to bring this into the JS rather than HTML, unsure how to target these arrows
 
 // Length function 
-
 function lengthConversion (number) {
     let meterToFeet = number * 3.281
     let feetToMeter = number / 3.281
 
-return lengthConversionResult.innerHTML = `${input.value}m = ${Number(meterToFeet).toFixed(3)} feet 
-| ${input.value}feet = ${Number(feetToMeter).toFixed(3)}m`
+return lengthConversionResult.innerHTML = `${number}m = ${Number(meterToFeet).toFixed(3)} feet 
+| ${number}feet = ${Number(feetToMeter).toFixed(3)}m`
 }
 
 // Volume function 
@@ -40,8 +38,8 @@ function volumeConversion (number) {
     let literToGallon = number / 3.785
     let gallonToLiter = number * 3.785
 
-return volumeConversionResult.innerHTML = `${input.value}L = ${Number(literToGallon).toFixed(3)} gal 
-| ${input.value}gal = ${Number(gallonToLiter).toFixed(3)}L`
+return volumeConversionResult.innerHTML = `${number}L = ${Number(literToGallon).toFixed(3)} gal 
+| ${number}gal = ${Number(gallonToLiter).toFixed(3)}L`
 }
 
 // Mass Function 
@@ -49,7 +47,27 @@ function massConversion (number) {
     let kgToPound = number * 2.205
     let poundToKg = number / 2.205
 
-
-return massConversionResult.innerHTML = `${input.value}kg = ${Number(kgToPound).toFixed(3)}lbs 
-| ${input.value}lbs = ${Number(poundToKg).toFixed(3)}kg`
+return massConversionResult.innerHTML = `${number}kg = ${Number(kgToPound).toFixed(3)}lbs 
+| ${number}lbs = ${Number(poundToKg).toFixed(3)}kg`
 }
+
+
+// function conversion (number) {
+    //     let meterToFeet = number * 3.281
+    //     let feetToMeter = number / 3.281
+    //     let literToGallon = number / 3.785
+    //     let gallonToLiter = number * 3.785
+    //     let kgToPound = number * 2.205
+    //     let poundToKg = number / 2.205
+        
+    //     return {
+    //     [lengthConversionResult.innerHTML = `${number}m = ${Number(meterToFeet).toFixed(3)} feet 
+    //     ${number}feet = ${Number(feetToMeter).toFixed(3)}m`]
+    
+    //     [volumeConversionResult.innerHTML = `${number}L = ${Number(literToGallon).toFixed(3)} gal 
+    //     ${number}gal = ${Number(gallonToLiter).toFixed(3)}L`],
+    
+    //    [massConversionResult.innerHTML = `${number}kg = ${Number(kgToPound).toFixed(3)}lbs 
+    //     ${number}lbs = ${Number(poundToKg).toFixed(3)}kg`]
+    //     }
+    // }
