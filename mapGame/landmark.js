4b694664d1f63f7c06667ef11fe9d6f3f5d2de6e
oilythighs <oly.marks@gmail.com>
2023-01-28 11:35:37 +1000

    landmark game

---
+++ mapGame/landmark.js
@@ -0,0 +1,181 @@
+import { landmarkData } from './landmarkData.js'
+
+// import { LookupRegionResponse } from "@googlemaps/region-lookup";
+let map
+let landmarks = {}
+let usedLandmarks = []
+let selectedLandmark = {}
+let userSelectedCountry
+let userMarker
+let marker
+let markerPlaced = false
+let markerCircle
+let correctMarker
+let geocoder
+let countryLabels = []
+// let countryLayer
+let score = 0
+
+
+
+const nextBtn = document.getElementById("next-btn")
+const question = document.getElementById("question")
+const submitBtn = document.getElementById("submit-btn")
+const scoreBox = document.getElementById("score")
+
+
+
+function initMap() {
+    selectedLandmark = getCountry()
+    if (typeof google !== 'undefined') {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: { lng: -21.58516515009815, lat: 52.53671745701234},  
+            zoom: 2,
+            mapId: 'a3924b58325ef9ed',
+            streetViewControl: false,
+            mapTypeControl: false,
+        });
+
+       
+        geocoder = new google.maps.Geocoder()
+        google.maps.event.addListener(map, 'click', function (event) {
+            if (!markerPlaced) {
+                placeUserMarker(event.latLng)
+                markerPlaced = true
+            }
+        })
+
+    }
+    if (!marker) {
+        marker = new google.maps.Marker({
+            position: { lat: selectedLandmark.latitude, lng: selectedLandmark.longitude },
+            map: map,
+            label: {
+                text: selectedLandmark.name,
+                className: 'marker-label'
+            },
+            animation: google.maps.Animation.Drop,
+            visible: false
+            });
+}
+}
+
+nextBtn.addEventListener("click", function () {
+    if (markerPlaced) {
+        userMarker.setMap(null);
+        userMarker = null
+        markerPlaced = false;
+        marker.setVisible(false);
+        markerCircle.setMap(null)
+        
+    }
+    map.setZoom(2)
+    map.panTo({ lng: -38.149403182714195, lat: 42.18812797137536})
+    selectedLandmark = getCountry()
+    marker.setPosition({ lat: selectedLandmark.latitude, lng: selectedLandmark.longitude });
+                marker.setLabel({
+                text: selectedLandmark.name,
+                className: 'marker-label',
+                });
+                marker.setVisible(false);
+               
+    question.innerHTML = `<img class="landmark-img" src ="${selectedLandmark.img}">`
+    question.classList.remove("correct", "incorrect")
+    submitBtn.disabled = true
+})
+
+submitBtn.addEventListener("click", function (){  
+  
+
+    checkAnswer()
+})
+
+
+
+function placeUserMarker(location) {
+    userMarker = new google.maps.Marker({
+        position: location,
+        draggable: true,
+        map: map,
+        animation: google.maps.Animation.Drop,
+    })
+    userMarker.addListener('dragend', function() {
+        geocode(userMarker.getPosition())
+      });
+    submitBtn.disabled = false
+}
+
+function getCountry() {
+    const landmarkKeys = Object.keys(landmarkData)
+    let randomIndex = Math.floor(Math.random() * landmarkKeys.length)
+    let landmarkKey = landmarkKeys[randomIndex]
+
+ // Keep selecting a new random key until one is found that hasn't been used yet
+ while (usedLandmarks.includes(landmarkKey)) {
+    randomIndex = Math.floor(Math.random() * landmarkKeys.length)
+    landmarkKey = landmarkKeys[randomIndex]
+}
+
+usedLandmarks.push(landmarkKey)
+
+
+    selectedLandmark = {
+        name: getNestedValue(landmarkData, landmarkKey, "name"),
+        img: getNestedValue(landmarkData, landmarkKey, "img"),
+        longitude: getNestedValue(landmarkData, landmarkKey, "longitude"),
+        latitude: getNestedValue(landmarkData, landmarkKey, "latitude")
+    }
+    question.innerHTML = `<img class="landmark-img" src =${selectedLandmark.img}>`
+    console.log(selectedLandmark)
+    return selectedLandmark 
+}
+
+
+
+function getNestedValue(obj, key, nestedKey) {
+    if (obj[key] && obj[key][nestedKey]) {
+        return obj[key][nestedKey]
+    }
+    return null
+}
+
+
+
+
+function checkAnswer() {
+    marker.setVisible(true);
+    let markerCircle = createCircle(marker)
+    markerCircle.setMap(map)
+    let isInside = markerCircle.getBounds().contains(userMarker.getPosition())
+    if (isInside) {
+        question.classList.add("correct")
+        score++
+    } else {
+    let distanceInMeters = google.maps.geometry.spherical.computeDistanceBetween(marker.getPosition(), userMarker.getPosition());
+        let distanceInKilometers = Math.floor(distanceInMeters / 1000)
+        document.getElementById("question").textContent = `${distanceInKilometers}km away`
+    question.classList.add("incorrect")
+    }
+    submitBtn.disabled = true
+    userMarker.setDraggable(false)
+    scoreBox.textContent = score
+
+}
+
+function createCircle(marker) {
+    markerCircle = new google.maps.Circle({
+        center: marker.getPosition(),
+        radius: 25000,  // radius in meters
+        fillColor: '#AA0000',
+        fillOpacity: 0.6,
+        strokeOpacity: 0.6,
+         strokeWeight: 2,
+        // clickable: false,
+        visible: true
+    });
+    return markerCircle;
+}
+
+initMap()
+
+
---
+++ mapGame/landmarkData.js
@@ -0,0 +1,266 @@
+export const landmarkData = [ 
+{
+    "name": "Forbidden City",
+    "country": "China",
+    "city": "Beijing",
+    "latitude": 39.916939171246476,
+    "longitude":116.39090266136692,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Forbidden_City_Beijing_Shenwumen_Gate.JPG/300px-Forbidden_City_Beijing_Shenwumen_Gate.JPG" ,
+},
+
+{
+    "name": "St. Peter's Basilica",
+    "country": "Vatican City",
+    "city": "Vatican City",
+    "latitude": 41.90210281736452, 
+    "longitude": 12.454344395745808,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Basilica_di_San_Pietro_in_Vaticano_September_2015-1a.jpg/300px-Basilica_di_San_Pietro_in_Vaticano_September_2015-1a.jpg" ,
+},
+
+{
+    "name": "Palace of Versailles",
+    "country": "France",
+    "city": "Versailles",
+    "latitude": 48.80496380718373, 
+    "longitude": 2.1207094495011694,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Parc_chateau_versailles.jpg/300px-Parc_chateau_versailles.jpg" ,
+}, 
+
+{
+    "name": "Lincoln Memorial",
+    "country": "United States of America",
+    "city": "Washington, D.C",
+    "latitude": 38.889368790357985, 
+    "longitude": -77.04981132201578, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Aerial_view_of_Lincoln_Memorial_-_east_side_EDIT.jpeg/480px-Aerial_view_of_Lincoln_Memorial_-_east_side_EDIT.jpeg" ,
+},
+
+{
+    "name": "Colossuem",
+    "country": "Italy",
+    "city": "Rome", 
+    "latitude": 41.89018623953389, 
+    "longitude": 12.492692239922889,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Colosseo_2020.jpg/480px-Colosseo_2020.jpg" ,
+},
+
+{
+    "name": "Parthenon",
+    "country": "Greece",
+    "city": "Athens",
+    "latitude": 37.97148619111531, 
+    "longitude": 23.72700627610699, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/da/The_Parthenon_in_Athens.jpg/480px-The_Parthenon_in_Athens.jpg" ,
+},
+
+{
+    "name": "Eiffel Tower",
+    "country": "France",
+    "city": "Paris",
+    "latitude": 48.85837713803054, 
+    "longitude": 2.294588586288651,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg/288px-Tour_Eiffel_Wikimedia_Commons_%28cropped%29.jpg" ,
+},
+
+{
+    "name": "Sagrada Familia",
+    "country": "Spain",
+    "city": "Barcelona",
+    "latitude": 41.40362183189312, 
+    "longitude": 2.1745489167008136,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Sagrada_Familia_2021.jpg/384px-Sagrada_Familia_2021.jpg" ,
+},
+
+{
+    "name": "Taj Mahal",
+    "country": "India",
+    "city": "Agra",
+    "latitude": 27.172073114870077,  
+    "longitude": 78.03973625127504,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Taj_Mahal_in_India_-_Kristian_Bertel.jpg/480px-Taj_Mahal_in_India_-_Kristian_Bertel.jpg" ,
+},
+
+{
+    "name": "Teotihuacán",
+    "country": "Mexico",
+    "city": "Teotihuacán",
+    "latitude": 19.697074828818828, 
+    "longitude": -98.83422035984279, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Teotihuac%C3%A1n-5973.JPG/480px-Teotihuac%C3%A1n-5973.JPG" ,
+},
+
+{
+    "name": "Pompei",
+    "country": "Italy",
+    "city": "Naples",
+    "latitude": 40.74792009985037, 
+    "longitude": 14.500891927399598, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Theathres_of_Pompeii.jpg/437px-Theathres_of_Pompeii.jpg" ,
+},
+
+{
+    "name": "Tower of London",
+    "country": "United Kingdom",
+    "city": "London",
+    "latitude": 51.50636457063853, 
+    "longitude": -0.07357547974611274, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Tower_of_London_from_the_Shard_%288515883950%29.jpg/480px-Tower_of_London_from_the_Shard_%288515883950%29.jpg" ,
+},
+
+{
+    "name": "Alhambra",
+    "country": "Spain",
+    "city": "Granada",
+    "latitude": 37.17612959126009, 
+    "longitude": -3.587862350279182,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/Dawn_Charles_V_Palace_Alhambra_Granada_Andalusia_Spain.jpg/480px-Dawn_Charles_V_Palace_Alhambra_Granada_Andalusia_Spain.jpg" ,
+},
+
+{
+    "name": "Moscow Kremlin",
+    "country": "Russia",
+    "city": "Moscow",
+    "latitude": 55.751956863282935, 
+    "longitude": 37.61759595773318, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Moscow_Kremlin_%288281675670%29.jpg/480px-Moscow_Kremlin_%288281675670%29.jpg" ,
+},
+
+{
+    "name": "Auschwitz",
+    "country": "Poland",
+    "city": "Oświęcim",
+    "latitude": 50.026851159711235, 
+    "longitude": 19.203437540309263, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/Brama_Arbeit_Macht_frei.jpg/320px-Brama_Arbeit_Macht_frei.jpg" ,
+},
+
+{
+    "name": "Neuschwanstein Castle",
+    "country": "Germany",
+    "city": "Hohenschwangau",
+    "latitude": 47.55748709497834, 
+    "longitude": 10.749939872745092, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Schloss_Neuschwanstein_2013.jpg/480px-Schloss_Neuschwanstein_2013.jpg" ,
+},
+
+{
+    "name": "Edinburgh Castle",
+    "country": "Scotland",
+    "city": "Edinburgh",
+    "latitude": 55.94857067012606,  
+    "longitude": -3.199656007950014,
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Edinburgh_Castle_from_the_south_east.JPG/480px-Edinburgh_Castle_from_the_south_east.JPG" ,
+},
+
+{
+    "name": "Machu Picchu",
+    "country": "Peru",
+    "city": "Cusco",
+    "latitude": -13.163162084613738, 
+    "longitude": -72.54472686861577, 
+    "img": "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Before_Machu_Picchu.jpg/480px-Before_Machu_Picchu.jpg" ,
+},
+
+{
+    "name": "Sydney Opera House",
+    "country":"Australia",
+    "city": "Sydney",
+    "longitude": 151.21539323879836,
+    "latitude": -33.85677540922206, 
+    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sydney_Australia._%2821339175489%29.jpg/375px-Sydney_Australia._%2821339175489%29.jpg", 
+},
+
+{
+    "name": "Pyramids of Giza",
+    "country":"Egypt",
+    "city": "Giza",
+    "longitude": 31.13431719100151,
+    "latitude": 29.97847938427422,
+    "img":"https://lh5.googleusercontent.com/p/AF1QipNdHs38V6d_sw9XanuxXd_gS4xN2vm66-cTKmbm=w408-h255-k-no", 
+},
+
+{
+    "name": "Petra",
+    "country":"Jordan",
+    "city": "Ma'an",
+    "longitude": 35.48255412471104,
+    "latitude": 30.32899752337905, 
+    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/The_Monastery%2C_Petra%2C_Jordan8.jpg/150px-The_Monastery%2C_Petra%2C_Jordan8.jpg", 
+},
+
+{
+    "name": "Christ the Redeemer",
+    "country":"Brazil",
+    "city": "Rio de Janeiro",
+    "longitude": -43.210336998954794,
+    "latitude": -22.951995020646567, 
+    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Unique_Moment_with_the_Moon_and_Christ_the_Redeemer_3.jpg/150px-Unique_Moment_with_the_Moon_and_Christ_the_Redeemer_3.jpg", 
+},
+
+
+{
+    "name": "Angkor Wat",
+    "country":"Cambodia",
+    "city": "Siam Reap",
+    "longitude": 103.86714662973041,
+    "latitude": 13.412427545718666, 
+    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Ankor_Wat_temple.jpg/330px-Ankor_Wat_temple.jpg", 
+},
+
+
+{
+    "name": "Burj Khalifa",
+    "country":"United Arab Emirates",
+    "city": "Dubai",
+    "longitude": 55.27435493969375,
+    "latitude": 25.197099904213594, 
+    "img":"https://upload.wikimedia.org/wikipedia/en/thumb/9/93/Burj_Khalifa.jpg/300px-Burj_Khalifa.jpg", 
+},
+
+
+{
+    "name": "Golden Gate Bridge",
+    "country":"United States of America",
+    "city": "San Francisco",
+    "longitude": -122.4785126292448,
+    "latitude": 37.819724871999334, 
+    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/GoldenGateBridge-001.jpg/450px-GoldenGateBridge-001.jpg", 
+},
+
+
+{
+    "name": "Stonehenge",
+    "country":"England",
+    "city": "Salisbury",
+    "latitude": -1.825785876043673,
+    "longitude": 51.17866644686866, 
+    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Stonehenge2007_07_30.jpg/330px-Stonehenge2007_07_30.jpg", 
+},
+
+
+{
+    "name": "Ha Long Bay",
+    "country":"Vietnam",
+    "city": "Ha Long",
+    "longitude": 107.17800762786581,
+    "latitude": 20.81811019926027, 
+    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Ha_Long_Bay_in_2019.jpg/330px-Ha_Long_Bay_in_2019.jpg", 
+},
+
+
+{
+    "name": "Portala Palace",
+    "country":"Tibet",
+    "city": "Lhasa",
+    "longitude": 91.11746336013866,
+    "latitude": 29.655456618273888, 
+    "img":"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Potala_Palace_%2823651397662%29.jpg/1280px-Potala_Palace_%2823651397662%29.jpg", 
+}
+
+
+
+
+
+
+
+]
